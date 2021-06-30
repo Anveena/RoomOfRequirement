8cef5c0a36f511545e43f830c92614cd54135af5
PanYS <zxxpys@qq.com>
2021-06-30 17:14:12 +0800

    add:增加一个设置TCP/UDP缓冲区并检测是否真的设置生效的功能

---
+++ ezNetworking/buffer_setting.go
@@ -0,0 +1,75 @@
+package ezNetworking
+
+import (
+	"errors"
+	"fmt"
+	"net"
+	"syscall"
+)
+
+func SetReadBufferSize(conn net.Conn, size int) error {
+	if udp, suc := conn.(*net.UDPConn); suc && udp != nil {
+		return setUDPBuffer(udp, size, true, false)
+	}
+	if tcp, suc := conn.(*net.TCPConn); suc && tcp != nil {
+		return setTCPBuffer(tcp, size, true, false)
+	}
+	return errors.New("unknown conn type")
+}
+func SetWriteBufferSize(conn net.Conn, size int) error {
+	if udp, suc := conn.(*net.UDPConn); suc && udp != nil {
+		return setUDPBuffer(udp, size, false, true)
+	}
+	if tcp, suc := conn.(*net.TCPConn); suc && tcp != nil {
+		return setTCPBuffer(tcp, size, false, true)
+	}
+	return errors.New("unknown conn type")
+}
+func setTCPBuffer(conn *net.TCPConn, size int, isRead bool, isWrite bool) error {
+	if isRead {
+		if err := conn.SetReadBuffer(size); err != nil {
+			return err
+		}
+	}
+	if isWrite {
+		if err := conn.SetWriteBuffer(size); err != nil {
+			return err
+		}
+	}
+	f, err := conn.File()
+	if err != nil {
+		return err
+	}
+	actuallyValue, err := syscall.GetsockoptInt(int(f.Fd()), syscall.SOL_SOCKET, syscall.SO_RCVBUF)
+	if err != nil {
+		return err
+	}
+	if actuallyValue != size {
+		return errors.New(fmt.Sprintf("set readbuffer failed,wanted result:%v,actually:%v", size, actuallyValue))
+	}
+	return nil
+}
+func setUDPBuffer(conn *net.UDPConn, size int, isRead bool, isWrite bool) error {
+	if isRead {
+		if err := conn.SetReadBuffer(size); err != nil {
+			return err
+		}
+	}
+	if isWrite {
+		if err := conn.SetWriteBuffer(size); err != nil {
+			return err
+		}
+	}
+	f, err := conn.File()
+	if err != nil {
+		return err
+	}
+	actuallyValue, err := syscall.GetsockoptInt(int(f.Fd()), syscall.SOL_SOCKET, syscall.SO_RCVBUF)
+	if err != nil {
+		return err
+	}
+	if actuallyValue != size {
+		return errors.New(fmt.Sprintf("set readbuffer failed,wanted result:%v,actually:%v", size, actuallyValue))
+	}
+	return nil
+}
